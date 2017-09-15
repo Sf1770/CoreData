d2b5d0376992b3c0ce8d556b8bc7052246f9f0f6
Sf1770 <sf1770@nyu.edu>
2017-09-15 01:39:19 -0400

    First time using Core Data

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Sabrina Fletcher on 9/14/17.
+//  Copyright © 2017 Sabrina Fletcher. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+
+}

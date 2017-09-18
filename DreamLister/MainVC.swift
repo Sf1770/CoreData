0497c94f4358570685b8f53f50f4487e2b33f637
Sf1770 <sf1770@nyu.edu>
2017-09-17 23:26:15 -0400

    Finished DreamLister App

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -21,27 +21,82 @@ class MainVC: UIViewController, UITableViewDataSource,UITableViewDelegate, NSFet
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+        //generateTestData()
+        attemptFetch()
     }
+    
+    
+    func configureCell(cell:ItemCell, indexPath: NSIndexPath){
+        //update cell
+        let item = controller.object(at: indexPath as IndexPath)
+        cell.configureCell(item: item)
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let objs = controller.fetchedObjects, objs.count > 0{
+            let item = objs[indexPath.row]
+            performSegue(withIdentifier: "ItemDetailsVC", sender: item)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ItemDetailsVC" {
+            if let destination = segue.destination as? ItemDetailsVC {
+                if let item = sender as? Item{
+                    destination.itemToEdit = item
+                }
+            }
+        }
+    }
+    
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
+        configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
+        return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let sections = controller.sections{
+            let sectionInfo = sections[section]
+            return sectionInfo.numberOfObjects
+        }
         return 0
     }
 
     
     func numberOfSections(in tableView: UITableView) -> Int {
+        if let sections = controller.sections{
+            return sections.count
+        }
+        
         return 0
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
     func attemptFetch(){
         let fetchRequest: NSFetchRequest<Item> = Item.fetchRequest()
         let dateSort = NSSortDescriptor(key: "created", ascending: false)
-        fetchRequest.sortDescriptors = [dateSort]
+        let priceSort = NSSortDescriptor(key: "price", ascending: true)
+        let titleSort = NSSortDescriptor(key: "title", ascending: true)
+        
+        if segment.selectedSegmentIndex == 0{
+            fetchRequest.sortDescriptors = [dateSort]
+        } else if segment.selectedSegmentIndex == 1{
+            fetchRequest.sortDescriptors = [priceSort]
+        } else if segment.selectedSegmentIndex == 2 {
+            fetchRequest.sortDescriptors = [titleSort]
+        }
         
         let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        
+        controller.delegate = self
+        self.controller = controller
+        
         
         do{
             try controller.performFetch()
@@ -53,6 +108,10 @@ class MainVC: UIViewController, UITableViewDataSource,UITableViewDelegate, NSFet
         
     }
     
+    @IBAction func segmentChanged(_ sender: Any) {
+        attemptFetch()
+        tableView.reloadData()
+    }
     
     func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         
@@ -82,6 +141,7 @@ class MainVC: UIViewController, UITableViewDataSource,UITableViewDelegate, NSFet
             if let indexPath = indexPath{
                 let cell = tableView.cellForRow(at: indexPath) as! ItemCell
                 //update the cell data
+                configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
             }
             break
         case.move:
@@ -94,6 +154,27 @@ class MainVC: UIViewController, UITableViewDataSource,UITableViewDelegate, NSFet
             break
         }
     }
+    
+    
+    func generateTestData(){
+        let item = Item(context: context)
+        item.title = "34inch 4k Curved monitor"
+        item.price = 1000
+        item.details = "Coding will be so much easier when I can split the screen on one big monitor."
+        
+        let item2 = Item(context: context)
+        item2.title = "Beats Studio Headphones"
+        item2.price = 200
+        item2.details = "Great for jamming out but also blocking out all the Bullshit."
+        
+        let item3 = Item(context: context)
+        item3.title = "Tesla Model X"
+        item3.price = 140000
+        item3.details = "This car lets me know that I have made it in life. That I am living comfortably and well beyond my means. Victory and Freedom."
+        
+        ad.saveContext()
+    }
+    
     
     
     
